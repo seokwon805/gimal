bc78a22fa1796dc9fc56b26f430e7dcee914bfed
seokwon805 <71585682+seokwon805@users.noreply.github.com>
2020-12-07 07:53:16 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$('.box').click(function(){
+
+	    $('.bg_1').css({ 
+    	'display':'none'
+		});
+
+		$('.bg_2').css({ 
+    	'display':'inline-block'
+		});
+
+	 })
+
+
+})
